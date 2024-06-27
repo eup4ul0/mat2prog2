4bbe4f60423fe851268e92904f3a0fc63d9d014c
eup4ul0 <129779805+eup4ul0@users.noreply.github.com>
2024-06-27 11:20:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const caixaPrincipal = document.querySelector(".caixa-Principal");
+const caixaPerguntas = document.querySelector(".caixa-Perguntas");
+const caixaAlternativa = document.querySelector(".caixa-Alternativa");
+const caixaResultado = document.querySelector(".caixa-Resultado");
+const textoResultado = document.querySelector(".texto-Resultado");
+
+const perguntas =[
+   {
+    enunciado: "pergunta 1",
+    alternativa: ["Alternativa 1", "alternativa 2"],
+
+   },
+   {
+    enunciado: "pergunta 2",
+    alternativa: ["Alternativa 1", "alternativa 2"],
+
+   },
+   {
+    enunciado: "pergunta 3",
+    alternativa: ["Alternativa 1", "alternativa 2"],
+
+   },
+
+
+]
+let atual = 0;
+let perguntaAtual;
+ function mostraPerguntas(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+ }
+
 
